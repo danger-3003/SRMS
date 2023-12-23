255c1f5db4408e51a53730eb3331b4f3a8223b83
danger-3003 <naremsumanth@gmail.com>
2023-12-23 21:00:13 +0530

    updated styles

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
+var login_page=document.getElementById("Login");
+var signup_page=document.getElementById("Signup");
+var signup_btn=document.getElementById("Signup_btn");
+var login_btn=document.getElementById("Login_btn");
 
-<div class="w-96 h-[30rem] bg-[#ffffff46] mx-auto my-20 rounded-lg" style="backdrop-filter: blur(8px);">
-            
-</div>
+// console.log(login_page,signup_page);
+signup_btn.addEventListener('click',()=>
+{
+    signup_page.classList.replace("left-[130%]","left-0");
+    login_page.classList.replace("right-[0%]","right-[130%]");
+})
+login_btn.addEventListener('click',()=>{
+    signup_page.classList.replace("left-0","left-[130%]");
+    login_page.classList.replace("right-[130%]","right-[0%]");
+})
